4cda949e9734e7c7f1035c4b648df2ad386ae3a4
Rohah Halarnkar <rohan.halarnkar@gmail.com>
2020-01-12 13:29:24 +0530

    This is my third commit

--- src/main/java/mavenProject/testHello.java
+++ src/main/java/mavenProject/testHello.java
@@ -3,7 +3,7 @@ package mavenProject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class testHello {
 
