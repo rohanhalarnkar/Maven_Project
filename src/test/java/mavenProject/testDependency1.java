fcd258361cfdaf801d805c2740e3b62146c7e9f3
Rohah Halarnkar <rohan.halarnkar@gmail.com>
2020-01-12 12:57:40 +0530

    This is my first commit

---
+++ src/test/java/mavenProject/testDependency1.java
@@ -0,0 +1,22 @@
+package mavenProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class testDependency1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+}
